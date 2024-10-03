42149e6f125fb6e36f81165dc36302388237c143
Secret-Pinecone <98505229+Secret-Pinecone@users.noreply.github.com>
2024-10-03 11:12:47 -0400

    Update bluetooth.h

--- RobotCar/bluetooth.h
+++ RobotCar/bluetooth.h
@@ -2,18 +2,18 @@
 #include <LCD_I2C.h>
 LCD_I2C lcd(0x27, 16, 2);
 
-#define BLUETOOTH_BAUD_RATE 9600
+#define BLUETOOTH_BAUD_RATE 38400
 
-#define Rx 15   //sets transmit pin on the bluetooth to the Rx pin on the arduino
+#define Rx 15  //sets transmit pin on the bluetooth to the Rx pin on the arduino
 #define Tx 14  //sets recieve pin on the bluetooth to the Tx pin on the arduino
 
 SoftwareSerial bluetooth(Rx,Tx);
 int bluetoothData;
 
 void setup() {
-  pinMode(Rx, INPUT);
-  pinMode(Tx, OUTPUT);
-  bluetooth.begin(BLUETOOTH_BAUD_RATE);
+  pinMode(Rx, INPUT);     //sets Rx as an input
+  pinMode(Tx, OUTPUT);    //sets Tx as an output
+  bluetooth.begin(BLUETOOTH_BAUD_RATE);     //starts bluetooth communication
 }
 String readCommand() {
   String asciiData = "";
@@ -21,8 +21,8 @@ String readCommand() {
     lcd.clear();
     while(bluetooth.available())
     {
-      bluetoothData = bluetooth.read();
-      if( bluetoothData != 13 && bluetoothData != 10)
+      bluetoothData = bluetooth.read();     //reads the bluetooth data
+      if( bluetoothData != 13 && bluetoothData != 10)   //ignores certain unwnated values
       {
         asciiData += (char)bluetoothData;
       }
@@ -31,6 +31,5 @@ String readCommand() {
   return asciiData;
 }
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
